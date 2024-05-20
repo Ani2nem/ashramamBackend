28ebe9d4068d15c9acd53c7f569133eb7bc628ed
Ani2nem <ani2nem@gmail.com>
2024-05-20 18:20:33 -0500

    added git to the backend

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+// set up express server
+const express = require('express');
+const app = express();
+
+// middleware
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3001',
+}));
+
+// set up bodyParser to read JSON data
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// import stripe and use it with our key
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);
+
+// api request 
+app.post('/create-payment-intent', async (req, res) => {
+    const { amount } = req.body;
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: 'usd',
+        });
+        res.status(200).send({
+            clientSecret: paymentIntent.client_secret,
+        });
+    } catch (error) {
+        res.status(500).send({
+            error: error.message,
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
